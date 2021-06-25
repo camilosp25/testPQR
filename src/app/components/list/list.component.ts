d7ef0bcb2daad96396453cd6226c2fc5ea2d37a3
Camilo Sanchez <camilosp_0525@hotmail.com>
2021-06-25 10:22:56 -0500

    busqueda de palabra clave

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -14,6 +14,7 @@ export class ListComponent implements OnInit {
   faSearch = faSearch;
   userId: number | null;
   pqrs: any[] = [];
+  filteredPqrs: any[] = [];
 
   constructor(private router: Router) {
     this.userId = parseInt(<string>sessionStorage.getItem('userId'));
@@ -21,10 +22,18 @@ export class ListComponent implements OnInit {
 
   ngOnInit(): void {
     this.pqrs = pqrs_data.filter((data: { userId: number | null; }) => data.userId == this.userId)[0].pqrs;
+    this.filteredPqrs = this.pqrs;
   }
 
-  pickOne(item: string){
+  pickOne(item: string) {
     this.router.navigate(['pages/pqrs-history', item]);
+  }
+
+  search(keyword: string) {
+    keyword = keyword.toLowerCase();
+    this.filteredPqrs = this.pqrs.filter(data => {
+      return data.code.toLowerCase().indexOf((keyword)) > -1 || data.date.toLowerCase().indexOf((keyword)) > -1 || data.subject.toLowerCase().indexOf((keyword)) > -1
+    });
   }
 
 }
